4ac324040d681a4463a6313dbc671ed0d8db27ae
alru2002 <115723072+alru2002@users.noreply.github.com>
2022-12-21 20:14:32 +0100

    Avance

--- Trabajo_P3/src/main/java/Testing/Trabajo_P3/GestorCuentas.java
+++ Trabajo_P3/src/main/java/Testing/Trabajo_P3/GestorCuentas.java
@@ -3,41 +3,35 @@ package Testing.Trabajo_P3;
 import java.util.ArrayList;
 
 public class GestorCuentas {
-private ArrayList<Cuenta>cuentas; 
+	private ArrayList<Cuenta> cuentas;
 
-public GestorCuentas() {
-	cuentas= new ArrayList<Cuenta>();
-}
+	public GestorCuentas() {
+		cuentas = new ArrayList<Cuenta>();
+	}
 
-public ArrayList<Cuenta> getCuentas() {
-	return cuentas;
-}
+	public ArrayList<Cuenta> getCuentas() {
+		return cuentas;
+	}
 
-public Cuenta NewCuenta(Cliente client) {
-	Cuenta cuenta = new Cuenta(client, "",0);
-	if(client.getEdad()<18 && client.isEstudia() && !client.isIndependizado()) {
-		cuenta.setTipo("Confort");
+	public Cuenta NewCuenta(Cliente client) {
+		Cuenta cuenta = new Cuenta(client, "", 0);
+		if (client.getEdad() < 18 && client.isEstudia() && !client.isIndependizado()) {
+			cuenta.setTipo("Confort");
+		} else if (client.getEdad() < 25 && client.isEstudia() && client.isIndependizado()) {
+			cuenta.setTipo("Vamos que tu puedes");
+		} else if (client.getEdad() > 18 && client.getEdad() < 25 && client.isTrabaja() && !client.isIndependizado()) {
+			cuenta.setTipo("Ahorra ahora que puedes");
+		} else if (client.getEdad() > 18 && client.getEdad() < 25 && client.isTrabaja() && client.isIndependizado()) {
+			cuenta.setTipo("Saltando del nido");
+		} else if (client.getEdad() > 25 && client.isTrabaja() && !client.isIndependizado()) {
+			cuenta.setTipo("Independizate que va siendo hora");
+		} else if (client.getEdad() > 25 && client.isTrabaja() && client.isIndependizado()) {
+			cuenta.setTipo("Bienvenido a la vida adulta");
+		} else {
+			cuenta.setTipo("ordinaria");
+		}
+		cuentas.add(cuenta);
+		return cuenta;
 	}
-	else if(client.getEdad()<25 && client.isEstudia() && client.isIndependizado()) {
-		cuenta.setTipo("Vamos que tu puedes");
-	}
-	else if(client.getEdad()>18 && client.isTrabaja() && !client.isIndependizado()) {
-		cuenta.setTipo("Ahorra ahora que puedes");
-	}
-	else if(client.getEdad()>18 && client.isTrabaja() && client.isIndependizado()) {
-		cuenta.setTipo("Saltando del nido");
-	}
-	else if(client.getEdad()>25 && client.isTrabaja() && !client.isIndependizado()) {
-		cuenta.setTipo("Independizate que va siendo hora");
-	}
-	else if(client.getEdad()>25 && client.isTrabaja() && client.isIndependizado()) {
-		cuenta.setTipo("Bienvenido a la vida adulta");
-	}
-	else if(client.getEdad()==25 || client.getEdad()==18) {
-		cuenta.setTipo("ordinaria");
-	}
-	cuentas.add(cuenta);
-	return cuenta;
-}
 
 }
--- Trabajo_P3/src/test/java/Testing/Trabajo_P3/AppTest.java
+++ Trabajo_P3/src/test/java/Testing/Trabajo_P3/AppTest.java
@@ -3,14 +3,226 @@ package Testing.Trabajo_P3;
 import org.junit.jupiter.api.Test;
 
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AppTest {
     
-  @Test
+  @Test()
   public void eachUse1() {
     assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",-5,true,true,true));
     
 }
+  @Test()
+  public void eachUse2() {
+	assertThrows(IndependenciaNoValidaException.class,() -> new Cliente("Paco",15,false,true,true));
+	    
+	}
+  @Test()
+  public void eachUse3()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",18,true,false,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+  public void eachUse4()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",20,true,false,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+  public void eachUse5()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",25,true,false,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+public void eachUse6()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",65,false,true,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+public void eachUse7() {
+	  assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",234,true,false,true));
+  }
+  @Test()
+  public void PairWise1() {
+    assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",-5,true,true,true));
+    
+}
+  @Test()
+  public void PairWise2() {
+    assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",-5,false,false,false));
+    
+}
+  @Test()
+  public void PairWise3() {
+		assertThrows(IndependenciaNoValidaException.class,() -> new Cliente("Paco",15,true,true,true));
+  }
+  
+  @Test()
+  public void PairWise4()throws IndependenciaNoValidaException,EdadNoValidaException  {
+	  Cliente client=new Cliente("Paco",15,false,false,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+  public void PairWise5()throws IndependenciaNoValidaException,EdadNoValidaException  {
+	  Cliente client=new Cliente("Paco",18,true,true,true);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("Vamos que tu puedes", cuent.getTipo());
+  }
+  
+  @Test()
+  public void PairWise6()throws IndependenciaNoValidaException,EdadNoValidaException  {
+	  Cliente client=new Cliente("Paco",18,false,false,false);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test()
+  public void PairWise7()throws IndependenciaNoValidaException,EdadNoValidaException  {
+	  Cliente client=new Cliente("Paco",18,true,true,true);
+	  GestorCuentas g=new GestorCuentas();
+	  Cuenta cuent=g.NewCuenta(client);
+	  assertEquals("Vamos que tu puedes", cuent.getTipo());
+  }
+  @Test
+  public void pairwise8()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",20,false,false,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+    }
+  @Test
+  public void pairwise9()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",25,true,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+    }
+  @Test
+  public void pairwise10()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",25,false,false,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+    }
+  @Test
+  public void pairwise11()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",65,true,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Bienvenido a la vida adulta", cuent.getTipo());
+    }
+  @Test
+  public void pairwise12()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",65,false,false,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+    }
+  @Test
+  public void pairwise13() {
+  	  assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",234,true,true,true));
+    }
+  @Test
+  public void pairwise14() {
+  	  assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",234,false,false,false));
+    }
+  @Test
+  public void Decisiones1()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",20,false,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Saltando del nido", cuent.getTipo());
+  }
+  @Test
+  public void Decisiones3()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",17,false,true,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+  }
+  @Test
+  public void Decisiones5()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",20,false,false,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Ahorra ahora que puedes", cuent.getTipo());
+  }
+  
+  @Test
+  public void Decisiones7()throws IndependenciaNoValidaException,EdadNoValidaException {
+	  Cliente client=new Cliente("Paco",20,false,false,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Ahorra ahora que puedes", cuent.getTipo());
+  }
+  
+  @Test
+  public void decisiones2()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",15,true,false,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Confort", cuent.getTipo());
+    }
+  @Test
+  public void decisiones4()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",20,true,true,false);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Vamos que tu puedes", cuent.getTipo());
+    }
+  @Test
+  public void decisiones6()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",20,false,false,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Ahorra ahora que puedes", cuent.getTipo());
+    }
+  @Test
+  public void decisiones8()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",20,false,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Saltando del nido", cuent.getTipo());
+    }
+  @Test
+  public void decisiones10()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",65,false,false,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Independizate que va siendo hora", cuent.getTipo());
+    }
+  @Test
+  public void decisiones12()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",65,false,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("Bienvenido a la vida adulta", cuent.getTipo());
+    }
+  @Test
+  public void decisiones14()throws IndependenciaNoValidaException,EdadNoValidaException {
+  	  Cliente client=new Cliente("Paco",25,false,true,true);
+  	  GestorCuentas g=new GestorCuentas();
+  	  Cuenta cuent=g.NewCuenta(client);
+  	  assertEquals("ordinaria", cuent.getTipo());
+    }
+  @Test
+  public void decisiones16() {
+  	  assertThrows(EdadNoValidaException.class,() -> new Cliente("Paco",234,true,true,true));
+    }
+  @Test
+  public void decisiones18() {
+  	  assertThrows(IndependenciaNoValidaException.class,() -> new Cliente("Paco",15,false,true,false));
+    }
 }
